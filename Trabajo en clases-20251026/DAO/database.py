600d8f742fef218f5993c153a0a0459e19031c33
matisuilvilla-web <matisuilvilla@gmail.com>
2025-10-28 11:17:01 -0300

    Delete Trabajo en clases-20251026 directory

--- Trabajo en clases-20251026/DAO/database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-class Database:
-    def __init__(self, db_name='personas.db'):
-        self.db_name = db_name
-
-    def connect(self):
-        return sqlite3.connect(self.db_name)
